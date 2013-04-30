f214d6038108c8b22cccd29dab6e101b7d7de718
Stefan Brehm <stef_brehm@web.de>
2013-04-30 17:19:18 +0200

    added new line

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
  * Copyright (C) 2011  Christian Ege
  */
 
+
+
+
 /* 
  * This software is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
